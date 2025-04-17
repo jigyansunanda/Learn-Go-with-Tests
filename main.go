9f9c242fca80873e7a0fa6a63ee6fda50476c128
jigyansunanda <nandajigyansu@gmail.com>
2025-04-17 06:45:50 +0530

    Refactor main function to include user input handling and option selection for hello world and integer summation

--- main.go
+++ main.go
@@ -1,9 +1,50 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+
 	helloworld "github.com/jigyansunanda/Learn-Go-with-Tests/go-fundamentals/01-hello-world"
+	adder "github.com/jigyansunanda/Learn-Go-with-Tests/go-fundamentals/02-integers"
 )
 
 func main() {
-	helloworld.Hello("Jigyansu")
+	// Create a reader for user input
+	reader := bufio.NewReader(os.Stdin)
+
+	fmt.Print("Choose your option between 1 to 2:\n")
+
+	// Read the user input
+	input, err := reader.ReadString('\n')
+	if err != nil {
+		fmt.Println("An error occurred while reading input. Please try again.")
+		return
+	}
+
+	// Trim any newline characters from the input
+	input = strings.TrimSpace(input)
+
+	// Convert the input string to an integer
+	option, err := strconv.Atoi(input)
+	if err != nil {
+		fmt.Println("Invalid input. Please enter an integer.")
+		return
+	}
+
+	switch option {
+	case 1:
+		fmt.Println(helloworld.Hello(""))
+		fmt.Println(helloworld.Hello("Jigyansu"))
+	case 2:
+		nums := make([]int, 0)
+		for i := range 5 {
+			nums = append(nums, i+1)
+			fmt.Printf("Slice: %v, Sum: %d\n", nums, adder.Add(nums...))
+		}
+	default:
+		fmt.Println("Looks like you are not ready to Go.😐")
+	}
 }
